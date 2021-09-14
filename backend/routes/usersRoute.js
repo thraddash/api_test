089fe0c0276dca5f44b557be858ae8a449f50be9
thraddash <theartist@gmail.com>
2021-09-13 19:45:46 -0700

    route backend data to frontend

---
+++ backend/routes/usersRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+router = express.Router();
+
+usersRoute = require("../controllers/usersControllers");
+
+router.get("/", usersRoute.usersController);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Express is online!")
-})
+app.use("/users/", require("./routes/usersRoute"));
 
 app.listen(3001, function() {
     console.log("express server is running on port 3001")
-})
+}) 
