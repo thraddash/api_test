70ab69d88656fafbb624dd6858fd856caf34580d
thraddash <theartist@gmail.com>
2021-09-17 05:46:04 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,44 +1,12 @@
 #!/usr/bin/env bash
 
-echo "HOST = $(hostname -I | awk '{print $1}')" > frontend/.env
-echo "PORT = 3001" >> frontend/.env
+echo "HOST = $(hostname -I | awk '{print $1}')" > /config/.env
+echo "PORT = 3001" >> /config/.env
 
-echo "HOST = $(hostname -I | awk '{print $1}')" > backend/.env
-echo "PORT = 3001" >> backend/.env
+#echo "HOST = $(hostname -I | awk '{print $1}')" > backend/.env
+#echo "PORT = 3001" >> backend/.env
 
-cat <<EOF > docker-compose.yml
-version: '3.8'
-
-services:
-  frontend:
-    image: frontend
-    ports:
-      - "80:80"
-    build:
-      dockerfile: Dockerfile
-      context: ./frontend
-  backend:
-    image: backend
-    ports:
-      - "3001:3001"
-    build:
-      dockerfile: Dockerfile
-      context: ./backend
-    depends_on:
-        - "db"
-    command: ["wait-for-it", "db:27017", "--", "node", "server.js"]
-  db:
-    image: mongo:4.2.0
-    volumes:
-      - "mongodata:/data/db"
-    ports:
-      - 27017:27017
-    command: mongod --noauth
-    
-volumes:
-  mongodata: 
-EOF
-#docker-compose build
+docker-compose config build
 
 #docker build . -t food2:latest
 #docker stop food2
