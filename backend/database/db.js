a6da12a9058c2111f0b445c5a0d22366786fdbc3
thraddash <theartist@gmail.com>
2021-09-17 17:18:45 -0700

    test4

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,8 @@
+//require('dotenv').config({ path: '../../.env' });
+
+//const MONGO_HOST = process.env.REACT_APP_MONGO_HOST
+
 module.exports = {
-    db: 'mongodb://localhost:27017/reactaxios'
+    //db: `mongodb://${MONGO_HOST}:27017/reactaxios`
+    db: `mongodb://localhost:27017/reactaxios`
 };
