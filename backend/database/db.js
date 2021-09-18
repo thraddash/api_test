0aa394a8dd367ece829843f9fb9e4639b264d244
thraddash <theartist@gmail.com>
2021-09-17 20:17:17 -0700

    need .env not exist use default settings

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,7 @@
-//require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: '../../.env' });
 
-//const MONGO_HOST = process.env.REACT_APP_MONGO_HOST
+const MONGO_HOST = process.env.REACT_APP_MONGO_HOST
 
 module.exports = {
-    //db: `mongodb://${MONGO_HOST}:27017/reactaxios`
-    db: `mongodb://localhost:27017/reactaxios`
+    db: `mongodb://${MONGO_HOST}:27017/reactaxios`
 };
--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo "REACT_APP_API_HOST=$(hostname -I | awk '{print $1}')" > frontend/.env
 echo "REACT_APP_API_PORT=3000" >> frontend/.env
 echo "REACT_APP_LOCALHOST=$(hostname -I | awk '{print $1}')" >> frontend/.env
 
-#echo "REACT_APP_MONGO_HOST=$(hostname -I | awk '{print $1}')" > backend/.env
-echo "REACT_APP_API_PORT=3001" > backend/.env
+echo "REACT_APP_MONGO_HOST=$(hostname -I | awk '{print $1}')" > backend/.env
+echo "REACT_APP_API_PORT=3001" >> backend/.env
 
 docker-compose down
 docker-compose build
